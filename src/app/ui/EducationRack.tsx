36fafb2710964e911b9ede29e46d05384aa3a149
Ben <kwalijkje@live.com>
2024-06-12 11:53:55 +0200

    Update EducationRack.tsx

--- src/app/ui/EducationRack.tsx
+++ src/app/ui/EducationRack.tsx
@@ -6,10 +6,10 @@ export interface EducationRackProps {
 const EducationRack = ({ instituteName, studied }: EducationRackProps) => {
   return (
     <div className="bg-secondcolor border-solid border-8 rounded-xl px-5 py-5 pt-1 pb-2.5">
-      <div className="font-semibold text-left text-thirdcolor">
+      <div className="font-semibold text-lg text-left md:text-white text-thirdcolor tracking-wide">
         <div>{instituteName}</div>
       </div>
-      <div className="font-semibold md:text-right text-white">
+      <div className="font-light text-right text-white tracking-wide">
         <div>{studied}</div>
       </div>
     </div>
