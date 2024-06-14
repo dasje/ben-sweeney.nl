171ac8de8d84fe948a8f4aaccd2c23459a3df522
Ben <kwalijkje@live.com>
2024-06-14 23:02:24 +0200

    Description of job added to experience

--- src/app/ui/ExperienceRack.tsx
+++ src/app/ui/ExperienceRack.tsx
@@ -14,15 +14,17 @@ const ExperienceRack = ({
   description,
 }: ExperienceRackProps) => {
   return (
-    <div className="bg-secondcolor border-solid border-8 rounded-xl px-5 py-5 pt-1 pb-2.5">
-      <div className="font-semibold text-lg text-left md:text-white text-thirdcolor tracking-wide">
+    <div className="grid gird-cols-1 md:grid-cols-2 bg-secondcolor border-solid border-8 rounded-xl px-5 py-5 pt-1 pb-2.5">
+      <div className="col-span-1 font-semibold text-lg text-left md:text-white text-thirdcolor tracking-wide">
         <div>{instituteName}</div>
+        <div className="font-light text-left text-white tracking-wide">
+          {description}
+        </div>
       </div>
-      <div className="font-light text-right text-white tracking-wide">
+      <div className="col-span-1 font-light text-right text-white tracking-wide">
         <div>{role}</div>
         <div>{years}</div>
         <div>{tools}</div>
-        <div>{description}</div>
       </div>
     </div>
   );
