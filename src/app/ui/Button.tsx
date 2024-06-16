ff357dfcdb42b28f6e15d8dc405eabafde5380b4
Ben <kwalijkje@live.com>
2024-06-16 21:11:26 +0200

    Update button props to handle wider range of button uses

--- src/app/ui/Button.tsx
+++ src/app/ui/Button.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "@/app/globals.css";
+import Image from "next/image";
+import backwardArrow from "@/app/lib/icons/icons8-back-arrow-100.png";
+import downwardArrow from "@/app/lib/icons/icons8-down-arrow-100.png";
+import forwardArrow from "@/app/lib/icons/icons8-right-arrow-100.png";
+import upwardArrow from "@/app/lib/icons/icons8-chevron-up-100.png";
 
 interface ButtonProps {
-  textToDisplay: string;
+  textToDisplay?: string;
   buttonColor?: "first" | "second" | "third" | "fourth" | "fifth" | "sixth";
   textColor?: "white" | "black";
   focusButton?: boolean;
+  direction?: "forward" | "backward" | "upward" | "downward";
   click: () => {} | void;
 }
 const Button = (props: ButtonProps) => {
+  const directionIcon = {
+    forward: ">",
+    backward: "<",
+    upward: "Up",
+    downward: "Down",
+  };
   const fontColor = {
     white: "text-white",
     black: "text-black",
@@ -27,16 +39,28 @@ const Button = (props: ButtonProps) => {
         props.buttonColor ? background[props.buttonColor] : "bg-fifthcolor"
       } border-solid ${
         props.focusButton ? "border-2 mix-blend-screen" : "border"
-      } rounded-full m-2.5 hover:animate-pulse hover:bg-bsButtonHoverColor`}
+      } rounded-full m-2.5 w-36 hover:animate-pulse hover:bg-bsButtonHoverColor flex justify-center`}
       onClick={props.click}
     >
-      <div
-        className={`font-semibold text-center ${
-          props.textColor ? fontColor[props.textColor] : "text-white"
-        } tracking-wider pl-5 pb-1 pr-5 pt-1`}
-      >
-        {props.textToDisplay}
-      </div>
+      {props.textToDisplay && (
+        <div
+          className={`font-semibold text-center ${
+            props.textColor ? fontColor[props.textColor] : "text-white"
+          } tracking-wider pl-5 pb-1 pr-5 pt-1`}
+        >
+          {props.textToDisplay}
+        </div>
+      )}
+      {props.direction && (
+        <div
+          className={`font-semibold text-center ${
+            props.textColor ? fontColor[props.textColor] : "text-white"
+          } tracking-wider pl-5 pb-1 pr-5 pt-1`}
+        >
+          {directionIcon[props.direction]}
+        </div>
+      )}
+      {}
     </div>
   );
 };
