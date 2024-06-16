ef1aa3a6b605fc6e96012e9e8031533bce0f7fb7
Ben <kwalijkje@live.com>
2024-06-16 21:11:43 +0200

    Added header background colors to twailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,10 @@ module.exports = {
         bsCardBackgroundColor: "rgba(15,76,92,0.9)",
         bsCardChildrenBackgroundColor: "rgba(255, 255, 255, 0.50)",
         bsButtonHoverColor: "rgb(22,108,130, 0.8)",
+        bsHeaderColorOne: "rgba(15,76,92,0.9)",
+        bsHeaderColorTwo: "rgba(16,59,92,0.9)",
+        bsHeaderColorThree: "rgba(92,31,15,0.9)",
+        bsHeaderColorFour: "rgba(37,92,15,0.9)",
       },
       fontFamily: {
         sans: ["Roboto", "Arial", "sans-serif"],
