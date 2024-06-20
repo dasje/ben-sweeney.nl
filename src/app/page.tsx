37e760dea6a2a50e3b876151f5d7d86c3ecd08a0
Ben <kwalijkje@live.com>
2024-06-20 16:06:17 +0200

    Slight rejig of sitemap to move About to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,30 @@
 "use client";
 
-// `app/page.tsx` is the UI for the `/` URL
+import Card from "@/app/ui/Card";
+import * as deets from "@/data/json/details.json";
+import detailsInterface from "@/data/interfaces/detailsInterface";
+import profilePic from "@/app/lib/images/profilepic.jpg";
+import Image from "next/image";
+
 export default function Page() {
-  return <div className="h-full overflow-auto"></div>;
+  const details = deets as detailsInterface;
+  return (
+    <Card title="About">
+      <div className="bg-thirdcolor p-5 rounded-2xl text-white decoration-wavy font-light tracking-wide grid grid-cols-3 space-y-4">
+        <div className="flex col-span-3 md:col-span-1 h-full w-full justify-center items-center">
+          <Image
+            className="rounded-full rounded-full ring-2 md:ring-8 ring-darkOrange"
+            style={{ width: "50%", height: "auto" }}
+            // width="200"
+            // height="200"
+            src={profilePic}
+            alt="profile pic"
+          />
+        </div>
+        <div className="col-span-3 md:col-span-2 w-4/5">
+          {details.aboutText}
+        </div>
+      </div>
+    </Card>
+  );
 }
